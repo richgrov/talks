fe3b3444ee388cefb53f0e9aba20efe54cc308ea
Richard Grover <38992201+richgrov@users.noreply.github.com>
2024-10-06 22:35:51 -0600

    Add tailwind to flask app

---
+++ htmx/flask/tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+	content: ["templates/*.html"],
+	theme: {
+		extend: {
+			colors: {
+				background: "var(--background)",
+				foreground: "var(--foreground)",
+			},
+		},
+	},
+	plugins: [],
+};
